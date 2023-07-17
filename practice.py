8bacdc36d983aa55d3b5a9cfabdb9607d8f50dfa
winter <jin-_-x@naver.com>
2023-07-17 18:31:28 +0900

    Practice for String

--- practice.py
+++ practice.py
@@ -57,3 +57,13 @@ sentence3 = """
 파이썬은 쉬워요
 """
 print(sentence3)
+
+
+jumin = "980619-1234567"
+print("성별: "+jumin[7])
+print("연: "+jumin[0:2])
+print("월: "+ jumin[2:4])
+print("일: "+jumin[4:6])
+print("생년월일: "+jumin[:6])
+print("뒤 7자리 : " +jumin[7:])
+print("뒤 7자리 (뒤에서부터) : "+jumin[-7:])
